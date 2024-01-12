c784096c03f8867cf332681a8d92ae40387c47ad
Ashutosh Prasad <45146282+ASHUTOSH57211@users.noreply.github.com>
2024-01-13 02:00:44 +0530

    initial++

---
+++ test/specs/TestLoginPage.js
@@ -0,0 +1,21 @@
+describe("Ecommerce Application", async()=>{
+
+    it("FailedLogingPage",async()=>{
+        await browser.url("https://rahulshettyacademy.com/loginpagePractise/")
+        console.log(await browser.getTitle())
+    }
+    )
+
+
+
+}
+
+
+
+
+
+
+
+
+
+)
